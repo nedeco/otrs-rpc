c05f2b10c492bec18228b6369b9fae7892f1dda0
Daniel Kempkens <nifoc@privatdemail.net>
2013-04-04 10:21:02 +0200

    Documentation

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('OTRS_BASE_URL',            'http://192.168.9.60/otrs');
-define('OTRS_USER',                'soap');
-define('OTRS_PASSWORD',            'seife');
-define('OTRS_DEFAULT_USER_ID',     1);
-define('OTRS_DEFAULT_QUEUE_ID',    1);
-define('OTRS_DEFAULT_PRIORITY_ID', 2);
-
